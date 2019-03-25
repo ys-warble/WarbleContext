2e56f9201e9607db2ea6182ac080c529319f5408
Yosef Saputra <yosef.saputra@gmail.com>
2019-03-25 01:14:34 -0500

    Not extract when the directory is not empty

--- dataset/dataset_extractor.py
+++ dataset/dataset_extractor.py
@@ -1,3 +1,4 @@
+import os
 import zipfile
 
 
@@ -14,10 +15,13 @@ class ZipExtractor:
             self.output_path = output_path
 
     def extract(self):
-        print('Extracting %s' % self.input_path)
-        zip_ref = zipfile.ZipFile(self.input_path, 'r')
-        zip_ref.extractall(self.output_path)
-        zip_ref.close()
+        if os.path.exists(self.output_path) and len(os.listdir(self.output_path)) != 0:
+            print('Skipping \'%s\': not empty' % os.path.basename(self.input_path))
+        else:
+            print('Extracting \'%s\' ...' % os.path.basename(self.input_path))
+            zip_ref = zipfile.ZipFile(self.input_path, 'r')
+            zip_ref.extractall(self.output_path)
+            zip_ref.close()
 
 
 if __name__ == '__main__':
